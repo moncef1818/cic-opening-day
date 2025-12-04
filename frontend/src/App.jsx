cd131c88bd1cd1da13ddad1929a498449118598d
Abderrahmane Mokaddem <1mokaddeme@gmail.com>
2025-12-04 07:29:44 +0100

    add complete login page with frontend/jwt authentication integration with back-end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from 'react'
-import { Routes,Route, useLocation} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'  
 import Home from './Pages/Home/Home'
-import Map from './Pages/Map/Map'
 import Login from './Pages/Login/Login'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
 import './App.css'
 
 function App() {
-
-  const location = useLocation();
-  const hideLayout = location.pathname === "/login"; // pages without navbar/footer
-
   return (
-    <>
-      {!hideLayout && <Navbar />}
-      
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/map' element={<Map />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-
-      {!hideLayout && <Footer />}
-    </>
-  );
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />  
+      <Route path="*" element={<Navigate to="/login" />} /> {/* for any other paths */}
+    </Routes>
+  )
 }
 
 export default App
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    // Check if logged in on component mount
+    const token = localStorage.getItem('access_token')
+    if (!token) {
+      navigate('/login')
+    }
+  }, [navigate])
+  
   return (
-    <div>Welcome to CIC Open Day!</div>
+    <div>
+      <h1>Welcome to Home Page!</h1>
+      {/* Your content */}
+    </div>
   )
 }
 
