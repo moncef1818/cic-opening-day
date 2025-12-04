8667169034228d981459fcf3a5b22566d547a836
Abderrahmane Mokaddem <1mokaddeme@gmail.com>
2025-12-04 17:48:03 +0100

    complete club registration page with responsive design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'  
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import ClubRegister from './Pages/Club-register/ClubRegister'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />  
+      <Route path="/club-register" element={<ClubRegister />} />
       <Route path="*" element={<Navigate to="/login" />} /> {/* for any other paths */}
     </Routes>
   )
