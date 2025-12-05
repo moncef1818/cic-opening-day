584c131aeb52a9503cfdfd72a57c9c9d22875579
mays-ro <rofaidataibi28@gmail.com>
2025-12-06 00:39:06 +0100

    the final version frontend for the book

---
+++ cloud.jsx
@@ -0,0 +1,38 @@
+
+import React, { useState } from 'react';
+import FlagPopupcloud from './FlagPopupcloud';
+import './cloud.css'
+
+const Cloud = () => {
+  const [isPopupOpencloud, setIsPopupOpencloud] = useState(false);
+  
+  const handleOpenPopupcloud = () => {
+    setIsPopupOpencloud(true);
+  };
+
+  const handleClosePopupcloud = () => {
+    setIsPopupOpencloud(false);
+  };
+
+  const handleSubmitFlagcloud = (flag) => {
+    console.log('Flag submitted:', flag);
+    setIsPopupOpencloud(false);
+    alert(`Flag submitted: ${flag}`);
+  };
+  
+  return (
+    <div className="app">
+      <button id='cloudbtn' onClick={handleOpenPopupcloud}>
+       
+      </button>
+      <FlagPopupcloud
+        isOpen={isPopupOpencloud}
+        onClose={handleClosePopupcloud}
+        onSubmit={handleSubmitFlagcloud}
+      />
+    </div>
+  );
+}
+
+export default Cloud;
+
