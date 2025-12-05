0d16dc03875fe105aa5194492eadd5b52c6175cf
mays Taibi <rofaidataibi28@gmail.com>
2025-12-05 10:31:52 +0100

    Add frontend popup pages and assets

---
+++ frontend/src/popup/marceline/marceline.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import FlagPopupmarceline from './FlagPopupmarceline';
+import './marceline.css'
+
+const Marceline = () => {
+  const [isPopupOpenmarceline, setIsPopupOpenmarceline] = useState(false);
+  
+  const handleOpenPopupmarceline = () => {
+    setIsPopupOpenmarceline(true);
+  };
+
+  const handleClosePopupmarceline = () => {
+    setIsPopupOpenmarceline(false);
+  };
+
+  const handleSubmitFlagmarceline = (flag) => {
+    console.log('Flag submitted:', flag);
+    setIsPopupOpenmarceline(false);
+    alert(`Flag submitted: ${flag}`);
+  };
+  
+  return (
+    <div className="app">
+      <button id='marcelinebtn' onClick={handleOpenPopupmarceline}>
+         marceline
+      </button>
+      <FlagPopupmarceline
+        isOpen={isPopupOpenmarceline}
+        onClose={handleClosePopupmarceline}
+        onSubmit={handleSubmitFlagmarceline}
+      />
+    </div>
+  );
+}
+
+export default Marceline;
